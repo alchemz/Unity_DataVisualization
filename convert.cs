6c13109d0a1e5512ae61b8524094c72cb3ed0f09
xianling zhang <alchemxz@gmail.com>
2017-02-14 11:59:41 -0500

    Create convert.cs

---
+++ convert.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ForceCSharpGitHub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a C# repository"); //Duplicate this line 500,000 times
+        }
+    }
+}
